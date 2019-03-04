0734b21ad454534788ff15d125ed2db4e9d0ed04
obviousrebel <jalexander@stsci.edu>
2019-03-04 13:26:29 -0500

    added jenkins file

--- pyxml.py
+++ pyxml.py
@@ -2,7 +2,7 @@ from junit_xml import TestSuite, TestCase
 
 test_cases = [TestCase('Cell Test', 'Notebook 1', 123.345, 'I am stdout!', 'I am stderr!')]
 test_cases[0].add_failure_info('Invalid File')
-ts = TestSuite("Notebook Tests", test_cases)
+ts = [TestSuite("Notebook Tests", test_cases)]
 # pretty printing is on by default but can be disabled using prettyprint=False
 print(TestSuite.to_xml_string(ts, prettyprint=False))
 
