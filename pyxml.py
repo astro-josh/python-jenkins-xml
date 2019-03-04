525e6b4e58dbcc4e028d1a01eaf6714d67653679
obviousrebel <jalexander@stsci.edu>
2019-03-04 13:08:53 -0500

    added jenkins file

--- pyxml.py
+++ pyxml.py
@@ -1,7 +1,8 @@
 from junit_xml import TestSuite, TestCase
 
-test_cases = [TestCase('Test1', 'some.class.name', 123.345, 'I am stdout!', 'I am stderr!')]
-ts = [TestSuite("my test suite", test_cases)]
+test_cases = [TestCase('Cell Test', 'Notebook 1', 123.345, 'I am stdout!', 'I am stderr!')]
+test_cases[0].add_failure_info('Invalid File \'DNC.exe\'.')
+ts = [TestSuite("Notebook Tests", test_cases)]
 # pretty printing is on by default but can be disabled using prettyprint=False
 print(TestSuite.to_xml_string(ts, prettyprint=False))
 
