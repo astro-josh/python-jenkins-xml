11f0e16a809733f3d1a2e2f594ccaf9c48595569
obviousrebel <jalexander@stsci.edu>
2019-03-04 14:33:18 -0500

    fixes

--- pyxml.py
+++ pyxml.py
@@ -2,6 +2,7 @@ from junit_xml import TestSuite, TestCase
 
 test_cases = [TestCase('Cell Test', 'Notebook 1', 123.345, 'I am stdout!', 'I am stderr!')]
 test_cases[0].add_failure_info('Invalid File')
+test_cases[0].elapsed_sec = 69
 ts = [TestSuite("Notebook Tests", test_cases)]
 
 with open('test-report.xml', mode='w') as file:
