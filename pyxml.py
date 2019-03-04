e4e51257a18725438760ed05027e39b9e4c4760e
obviousrebel <jalexander@stsci.edu>
2019-03-04 12:47:06 -0500

    init commit

---
+++ pyxml.py
@@ -0,0 +1,10 @@
+from junit_xml import TestSuite, TestCase
+
+test_cases = [TestCase('Test1', 'some.class.name', 123.345, 'I am stdout!', 'I am stderr!')]
+ts = [TestSuite("my test suite", test_cases)]
+# pretty printing is on by default but can be disabled using prettyprint=False
+print(TestSuite.to_xml_string(ts, prettyprint=False))
+
+with open('test-report.xml', mode='a') as file:
+    TestSuite.to_file(file, ts, prettyprint=False)
+    file.close()
