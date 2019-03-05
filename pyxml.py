3422c6a8585fa9231e60a2fec2a01cd0acb1cec1
obviousrebel <jalexander@stsci.edu>
2019-03-05 09:22:47 -0500

    added second test report

--- pyxml.py
+++ pyxml.py
@@ -3,6 +3,6 @@ from junit_xml import TestSuite, TestCase
 test_cases = [TestCase('Cell Test', 'Notebook 1', 123.345, 'I am stdout!', 'I am stderr!')]
 ts = [TestSuite("Notebook Tests", test_cases)]
 
-with open('output.xml', 'wb') as f:
+with open('output.xml', 'w') as f:
     TestSuite.to_file(f, ts, prettyprint=False)
     f.close()
