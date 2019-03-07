a9f0afc566a93af342aaeeed0df64b846964b3e5
obviousrebel <jalexander@stsci.edu>
2019-03-07 09:40:40 -0500

    new test report

--- pep8.py
+++ pep8.py
@@ -17,12 +17,24 @@ code = [
 "shutil.rmtree('science/')"
 ]
 
+source = [
+"job = HSC_service.launch_job(\"\"\"\n",
+"SELECT TOP 10 MatchID, MatchRA, MatchDec, TargetName, StartTime, StopTime, TargetName \n",
+"FROM dbo.SumMagAper2CatView\n",
+"WHERE \n",
+"CONTAINS(POINT('ICRS', MatchRA, MatchDec),CIRCLE('ICRS',129.23,7.95,0.1))=1\n",
+"AND StartTime > '2015-01-01' AND StopTime < '2015-04-01'\n",
+"\"\"\")\n",
+"HSC_results = job.get_results()\n",
+"HSC_results"
+]
 
-c = Checker(lines=code, quiet=0, ignore='W292')
+
+c = Checker(lines=source, quiet=0, ignore='W292')
 c.check_all()
 report = c.report
-print(len(report._deferred_print))
 l = []
+print(type(source))
 for line_number, offset, code, text, doc in report._deferred_print:
     if line_number > len(report.lines):
         line = ''
@@ -33,7 +45,6 @@ for line_number, offset, code, text, doc in report._deferred_print:
         'row': report.line_offset + line_number, 'col': offset + 1,
         'code': code, 'text': text, 'source': line.rstrip() + "\n" + re.sub(r'\S', ' ', line[:offset]) + '^',
     })
-    #print(line.rstrip())
-    #print(re.sub(r'\S', ' ', line[:offset]) + '^')
-print("\n\n\n")
+
+print("\n\nstart of list")
 print('\n\n'.join(l))
