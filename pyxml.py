b382172b9cf02dab9796262410e6c4756a209cea
obviousrebel <jalexander@stsci.edu>
2019-03-13 15:28:58 -0400

    testing show file changes

--- pyxml.py
+++ pyxml.py
@@ -1,15 +1,11 @@
-from junit_xml import TestSuite, TestCase
+
 import traceback
 import subprocess
 import re
 import html
 
-test_cases = [TestCase('Cell Test', 'Notebook_name', 123.345, 'I am stdout!', 'I am stderr!', file='./this.py')]
-ts=[TestSuite("Notebook Tests", test_cases, package="Notebooks")]
 
-# with open('output.xml', 'w') as f:
-#     TestSuite.to_file(f, ts, prettyprint=False)
-#     f.close()
+
 
 list_changed_files_cmd = "git show --pretty=format: --name-only master"
 
