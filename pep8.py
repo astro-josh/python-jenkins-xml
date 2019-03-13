2b22ff997f9d4c3c1862082b90e51e032be3ff40
obviousrebel <jalexander@stsci.edu>
2019-03-13 15:22:50 -0400

    testing show file changes

--- pep8.py
+++ pep8.py
@@ -2,6 +2,7 @@ from pycodestyle import Checker
 import re
 
 
+
 code = [
 "# Retrieve the IR/F160W calibrated FLT and SPT data products  \n",
 "\n",
--- pyxml.py
+++ pyxml.py
@@ -1,5 +1,6 @@
 from junit_xml import TestSuite, TestCase
 import traceback
+import subprocess
 import re
 import html
 
@@ -9,3 +10,9 @@ ts=[TestSuite("Notebook Tests", test_cases, package="Notebooks")]
 # with open('output.xml', 'w') as f:
 #     TestSuite.to_file(f, ts, prettyprint=False)
 #     f.close()
+
+list_changed_files_cmd = "git show --pretty=format: --name-only master"
+
+changed_file_names = subprocess.check_output(list_changed_files_cmd, shell=True).decode()
+changed_file_names = [x for x in changed_file_names.strip().split('\n') if '.py' in x]
+print(','.join(changed_file_names))
